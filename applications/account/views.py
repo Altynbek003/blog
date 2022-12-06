9d7db9235953df3361a6054f25b41abe591bf101
Altynbek003 <imanbaevaltynbek03@gmail.com>
2022-12-06 21:10:40 +0600

    first commit

---
+++ applications/account/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth import get_user_model
+from applications.account.serializers import RegisterSerializer
+
+User = get_user_model()
+
+class UserRegisterAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
